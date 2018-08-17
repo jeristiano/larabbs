00f0a266cc42f720697d99edd9a10cc471a42633
jeristiano <jeremykuang@foxmail.com>
2018-08-17 10:19:01 +0000

    上传文件/和容器

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,8 +18,15 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,User $user){
-        $user->update($request->all());
+    public function update(UserRequest $request,User $user,ImageUploadHandler $uploader){
+        $data = $request->all();
+        if($request->avatar){
+            $result=$uploader->save($request->avatar,'avatar',$user->id,362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('user.show',$user->id)->with('success','个人资料更新成功！');
     }
 }
