5a790a4200b9277299dc5a937e835419c26c4150
jeristiano <jeremykuang@foxmail.com>
2018-08-22 09:22:25 +0000

    generated topics

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Auth::routes();
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
